fee0c864ade8ecca58aafd98f62fdd09880ebc5b
Matias Gutierrez <mgutierrezl1402@gmail.com>
2024-11-08 20:45:16 -0300

    edited base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  base: '',
+  base: 'Brufit',
   site: 'https://matigutierrez.github.io',
 });
